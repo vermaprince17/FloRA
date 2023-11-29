af0d0d2a482d518e18746cbebfddd53af5e82024
lavrenti <mikaelya@usc.edu>
2023-11-28 20:16:56 -0800

    path

--- evaluate.py
+++ evaluate.py
@@ -387,7 +387,7 @@ def infer(inferencer,
     return result
 
 if __name__ == "__main__":
-    open_flamingo_path = "/content/checkpoint.pt"
+    open_flamingo_path = "checkpoints/checkpoint.pt"
     args, leftovers = parser.parse_known_args()
 
     
